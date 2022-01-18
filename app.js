8f1a90a597a9a34ffe7a1f3db61f2afb7a3791b1
82wa <b0021035@ib.yic.ac.jp>
2022-01-18 11:25:37 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 'use strict';
+
+const memo = new Map();
+memo.set(0, 0);             //0番目というキーに0という答えをセット
+memo.set(1, 1);
+function fib(n) {
+    if (memo.has(n)) {      //もしmemoがnをキーとした答えを持っていたら
+        return memo.get(n)  //その答えを返す
+    }
+    const value = fib(n - 1) + fib(n - 2);  //nというキーを持っていなければ計算
+    memo.set(n, value);
+    return value;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
+
+
+/*
 function fib(n) {
     if (n === 0) {
         return 0;
@@ -11,3 +30,4 @@ const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(fib(i));
 }
+*/
